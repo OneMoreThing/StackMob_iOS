081cd3edaeead48b864c5c7439ee8ab110041530
Douglas Rapp <drapp@stackmob.com>
2012-02-14 10:46:30 -0800

    Got the url format wrong

--- StackMob/StackMobSession.h
+++ StackMob/StackMobSession.h
@@ -16,7 +16,7 @@
 #import "StackMobConfiguration.h"
 #import "StackMobVersion.h"
 
-static NSString *const SMDefaultDomain = @"stackmob.com";
+static NSString *const SMDefaultDomain = @"api.mob1.stackmob.com";
 
 @interface StackMobSession : NSObject {
     NSString *url;
